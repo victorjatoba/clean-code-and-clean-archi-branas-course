8a1ea5604bec5774709ca88f2fb9d8eb349371b3
Victor Jatobá <victorjatoba10@gmail.com>
2022-03-07 23:15:31 -0300

    add test_patterns should create debit

--- class-02/test_patterns/src/Account.ts
+++ class-02/test_patterns/src/Account.ts
@@ -1,11 +1,19 @@
 export default class Account {
-    private balance: number;
-    
-    constructor(balance: number) {
-        this.balance = balance;
-    }
-    
-    public getBalance(): number {
-        return this.balance;
-    }
+  private balance: number;
+
+  constructor(balance: number) {
+    this.balance = balance;
+  }
+
+  credit(amount: number): void {
+    this.balance += amount;
+  }
+
+  debit(amount: number) {
+    this.balance -= amount;
+  }
+
+  public getBalance(): number {
+    return this.balance;
+  }
 }
--- class-02/test_patterns/test/account.test.ts
+++ class-02/test_patterns/test/account.test.ts
@@ -1,6 +1,14 @@
 import Account from "../src/Account";
+
 test("Should create an account", () => {
   const account = new Account(0);
   const balance = account.getBalance();
   expect(balance).toBe(0);
 });
+
+test("Should create a debit of $ 50.0", () => {
+    const account = new Account(100);
+    account.debit(50);
+    const balance = account.getBalance();
+    expect(balance).toBe(50);
+})
