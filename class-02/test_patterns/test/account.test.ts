9382f26989361d4d3d0703748c12e92a19b8da12
Victor Jatobá <victorjatoba10@gmail.com>
2022-03-08 23:11:02 -0300

    add mock test pattern example

--- class-02/test_patterns/test/account.test.ts
+++ class-02/test_patterns/test/account.test.ts
@@ -15,12 +15,6 @@ test("Should create an account", () => {
   expect(balance).toBe(0);
 });
 
-test("Should create a credit of $ 100.0", () => {
-  account.credit(100);
-  const balance = account.getBalance();
-  expect(balance).toBe(100);
-});
-
 test("Should create a debit of $ 50.0", () => {
   account.credit(100);
   account.debit(50);
@@ -28,9 +22,33 @@ test("Should create a debit of $ 50.0", () => {
   expect(balance).toBe(50);
 });
 
-test("Should create a debit of R$ 50.0 BRL", () => {
+test("Should create a credit of $ 100.0 with fake", () => {
+  account.credit(100);
+  const balance = account.getBalance();
+  expect(balance).toBe(100);
+});
+
+test("Should create a credit of R$ 50.0 BRL with stub", () => {
   sinon.stub(currencyAPI, "convert").returns(500);
   account.credit(100, "BRL");
   const balance = account.getBalance();
   expect(balance).toBe(500);
+});
+
+test("Should create a spy", () => {
+  const spy = sinon.spy(account, "getBalance");
+  account.getBalance();
+  sinon.assert.calledOnce(spy);
+});
+
+test("Should create a mock like stub", () => {
+  const mock = sinon.mock(account);
+  mock.expects("getBalance").once().returns(500);
+  const balance = account.getBalance();
+  expect(balance).toBe(500);
+});
+test("Should create a mock like spy", () => {
+  const mock = sinon.mock(account);
+  mock.expects("credit").once().withArgs(100);
+  account.credit(100);
 });
