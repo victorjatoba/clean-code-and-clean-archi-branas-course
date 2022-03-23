abea8790b0b98776c327e929696cf5d01f44d1ec
Victor Jatobá <victorjatoba10@gmail.com>
2022-03-22 21:36:03 -0300

    add order with cpf validator

--- class-02/implementacao_projeto_p1/src/Order.ts
+++ class-02/implementacao_projeto_p1/src/Order.ts
@@ -1,5 +1,10 @@
+import Cpf from "./Cpf";
+
 export default class Order {
-  constructor(private cpf: string) {}
+  cpf: Cpf;
+  constructor(cpf: string) {
+    this.cpf = new Cpf(cpf);
+  }
 
   public getTotal(): number {
     return 0;
--- class-02/implementacao_projeto_p1/test/Order.test.ts
+++ class-02/implementacao_projeto_p1/test/Order.test.ts
@@ -1,8 +1,13 @@
 import Order from "../src/Order";
 
 test("Deve criar um pedido com CPF válido", () => {
-    const cpf = "123.456.789-10";
-    const order = new Order(cpf)
-    const total = order.getTotal();
-    expect(total).toBe(0);
-})
+  const cpf = "487.501.680-88";
+  const order = new Order(cpf);
+  const total = order.getTotal();
+  expect(total).toBe(0);
+});
+
+test("Deve criar um pedido vazio com CPF inválido", () => {
+  const cpf = "111.111.111-11";
+  expect(() => new Order(cpf)).toThrow(new Error("Invalid CPF"));
+});
